5f68b62e68a6e3b4e36bffe42fa2a513690cc3cb
mustafa-al-shaheri <mustafa.alshaheri@gmail.com>
2020-01-15 02:52:01 -0800

    commit

--- index.js
+++ index.js
@@ -26,14 +26,14 @@ document.getElementById("page").style.display="block"
     document.getElementById("about").style.display="none"
     document.getElementById("skill").style.display="none"
     document.getElementById("experience").style.display="none"
-    document.getElementById("contact").style.display="none"
+    document.getElementById("contactUs").style.display="none"
 let skill1=document.getElementById("skill1")
 skill1.addEventListener('click',function(){
     document.getElementById("page").style.display="none"
     document.getElementById("about").style.display="none"
     document.getElementById("skill").style.display="block"
     document.getElementById("experience").style.display="none"
-    document.getElementById("contact").style.display="none"
+    document.getElementById("contactUs").style.display="none"
     console.log("skill1")
 
 })
@@ -44,7 +44,7 @@ about1.addEventListener('click',function(){
     document.getElementById("about").style.display="block"
     document.getElementById("skill").style.display="none"
     document.getElementById("experience").style.display="none"
-    document.getElementById("contact").style.display="none"
+    document.getElementById("contactUs").style.display="none"
     console.log("about1")
 })
 let home1=document.getElementById("home1")
@@ -53,7 +53,7 @@ home1.addEventListener('click',function(){
     document.getElementById("about").style.display="none"
     document.getElementById("skill").style.display="none"
     document.getElementById("experience").style.display="none"
-    document.getElementById("contact").style.display="none"
+    document.getElementById("contactUs").style.display="none"
     console.log("home1")
 })
 let experience1=document.getElementById("experience1")
@@ -62,7 +62,7 @@ experience1.addEventListener('click',function(){
     document.getElementById("about").style.display="none"
     document.getElementById("skill").style.display="none"
     document.getElementById("experience").style.display="block"
-    document.getElementById("contact").style.display="none"
+    document.getElementById("contactUs").style.display="none"
     console.log("experience1")
 })
 let contact1=document.getElementById("contact1")
@@ -71,7 +71,7 @@ contact1.addEventListener('click',function(){
     document.getElementById("about").style.display="none"
     document.getElementById("skill").style.display="none"
     document.getElementById("experience").style.display="none"
-    document.getElementById("contact").style.display="block"
+    document.getElementById("contactUs").style.display="block"
 console.log("contact1")
 })
 let imageM=document.getElementById("imageM")
@@ -80,7 +80,7 @@ imageM.addEventListener('click',function(){
     document.getElementById("about").style.display="none"
     document.getElementById("skill").style.display="none"
     document.getElementById("experience").style.display="none"
-    document.getElementById("contact").style.display="none"
+    document.getElementById("contactUs").style.display="none"
     console.log("M")
 })
 
@@ -131,3 +131,126 @@ $('.mouseover5').mouseover(function(){
 $('.mousehover5').mouseleave(function(){
     $('.contact1').hide(),$('.mouseover5').show();;
 });
+
+
+let btn =document.querySelector("#btn").value;
+let fName =document.querySelector("#fName").value;
+let lName =document.querySelector("#lName").value;
+let email =document.querySelector("#email").value;
+let phone =document.querySelector("#phone").value;
+let message =document.querySelector("#message").value;
+
+btn.addEventListener('click',function(){
+  var newContact= new contact(fName,lName,email,phone,message);
+
+console.log(fnm(fName.value),lnm(lName.value),em(email.value),mob(phone.value),mesg(message.value));
+//
+
+
+  event.preventDefault();
+});
+
+
+function fnm(v){
+  if(v.length>=3&&v.length<=25){
+    let number=2;
+    for(let i=0;i<v.length;i++){
+        if((isNaN(v[i]) ==false ||v[i]==="@" ||v[i]==="#" ||v[i]==="!" ||v[i]==="`" ||v[i]==="~" ||v[i]==="$" ||v[i]==="%" ||v[i]==="^" ||v[i]==="&" ||v[i]==="*" ||v[i]==="=" ||v[i]==="+" ||v[i]==="/" ||v[i]==="|") && v[i]!=" " ){
+            number = 3;
+        };
+        /*if(typeof Number(v[i]) =="number"  ){
+            sym = 3;
+            
+        }*/
+        
+    };
+    if(number!=3){
+        return v;
+    }
+    else if(number==3){
+      alert("Warning Unvalid Charecters in the first name")
+    }
+  }
+  else if(v.length>25){
+    alert("First name is longer than 25 charecters")
+  }
+  else if(v.length<3){
+    alert("First name is smaller than 3 charecters")
+  }
+}
+function lnm(v){
+  if(v.length>=3&&v.length<=25){
+    let number=2;
+    for(let i=0;i<v.length;i++){
+        if((isNaN(v[i]) ==false ||v[i]==="@" ||v[i]==="#" ||v[i]==="!" ||v[i]==="`" ||v[i]==="~" ||v[i]==="$" ||v[i]==="%" ||v[i]==="^" ||v[i]==="&" ||v[i]==="*" ||v[i]==="=" ||v[i]==="+" ||v[i]==="/" ||v[i]==="|") && v[i]!=" " ){
+            number = 3;
+        };
+        /*if(typeof Number(v[i]) =="number"  ){
+            sym = 3;
+            
+        }*/
+        
+    };
+    if(number!=3){
+        return v;
+    }
+    else if(number==3){
+      alert("Warning Unvalid Charecters in the last name")
+    }
+  }
+  else if(v.length>25){
+    alert("Last name is longer than 25 charecters")
+  }
+  else if(v.length<3){
+    alert("Last name is smaller than 3 charecters")
+  }
+}
+function em(e){
+  let a= e.split("@");
+  if(a.length==2){
+      let b= a[0].split(".");
+      let c=a[1].split(".");
+      if(b.length<=2&&c.length<=4){
+          return e;
+      }
+      else{
+        alert ("Unvalid Email");
+      }
+      
+  }
+  else{
+    alert ("Unvalid Email");
+  }
+}
+function mob(ss){
+  if(((ss[0]==7)&&(ss[1]==0||ss[1]==3||ss[1]==1||ss[1]==7))&&ss.length==9){
+      return ss;
+  }
+  else{
+    alert ("Not valid Yemeni Mobile Phone Number");
+  }
+};
+function mesg(a){
+  if(a.length>=10&&a.length<=330){
+    return a;
+  }
+  else if(a.length<10){
+    alert("message is too short");
+  }
+  else if(a.length>330){
+    alert("message is too long");
+  }
+}
+
+class contact{
+  constructor(firstName,lastName,email,phone,message){
+    this.firstName=firstName;
+    this.lastName=lastName;
+    this.email=email;
+    this.phone=phone;
+    this.message;
+  }
+}
+
+var newContact= new contact(fName,lName,email,phone,message);
+
